7542e7e46937e587488deb867bda0799381edbb1
Trần Đức Thắng <ducthang4825@gmail.com>
2023-10-07 10:51:50 +0700

    feat: add approve register action

--- src/app/(admin)/admin/registration/[id]/page.tsx
+++ src/app/(admin)/admin/registration/[id]/page.tsx
@@ -1,55 +1,93 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { formatDateTimeToddMMyyyyHHmm, formatDateToddMMyyyy } from "@/lib/format-datetime";
-import { RegistrationUseQuery } from "@/server/queries/registration-query";
-import { useParams } from "next/navigation";
+import { toast } from "@/components/ui/use-toast";
+import { registrationStatusToString } from "@/lib/convert";
+import { formatDateTimeToddMMyyyyHHmm } from "@/lib/format-datetime";
+import { RegistrationUseMutation } from "@/server/actions/registration-actions";
+import { useParams, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
-function RegistrationDetail({
-  params,
-  searchParams,
-}: {
-  params: {
-    id: string | number;
-  };
-  searchParams: {
-    fullname: string;
-    phone: string;
-    email: string;
-    address: string;
-    status: string;
-    createdAt: string;
-  };
-}) {
-  // const {data, isError, isFetched} = RegistrationUseQuery.getOneById(id as string);
-
+function RegistrationDetail() {
+  const params = useParams();
+  const searchParams = useSearchParams();
   console.log(params, searchParams);
+  const { mutateAsync } = RegistrationUseMutation.approve();
+  const [isLoading, setIsLoading] = useState(false);
+  async function handleApprove() {
+    try {
+      setIsLoading(true);
+      const res = await mutateAsync({ registration_id: params.id as string });
+      setIsLoading(false);
+      if (res.status === 200) {
+        toast({
+          title: "Thành công",
+          description: "Đã chấp nhận hồ sơ đăng ký",
+          variant: "success",
+        });
+      } else {
+        toast({
+          title: "Thất bại",
+          description: "Đã có lỗi xảy ra",
+          variant: "destructive",
+        });
+      }
+    } catch (error) {
+      toast({
+        title: "Đã có lỗi xảy ra",
+        description: "Vui lòng thử lại sau.",
+        variant: "destructive",
+      });
+    }
+  }
   return (
     <div className="w-full lg:w-2/3 p-10">
-        <h3 className="text-xl font-bold mb-10">Chi tiết hồ sơ đăng ký</h3>
+      <h3 className="text-xl font-bold mb-10">Chi tiết hồ sơ đăng ký</h3>
       <div className="grid grid-cols-3 mb-20">
         <Label className="col-span-1">Tên người đăng ký</Label>
         <span className="text-gray-500 col-span-2">
-          {searchParams?.fullname}
+          {searchParams.get("fullname")}
         </span>
         <Label className="col-span-1">Số điện thoại</Label>
-        <span className="text-gray-500 col-span-2">{searchParams?.phone}</span>
+        <span className="text-gray-500 col-span-2">
+          {searchParams.get("phone")}
+        </span>
         <Label className="col-span-1">Email</Label>
-        <span className="text-gray-500 col-span-2">{searchParams?.email}</span>
+        <span className="text-gray-500 col-span-2">
+          {searchParams.get("email")}
+        </span>
         <Label className="col-span-1">Địa chỉ</Label>
         <span className="text-gray-500 col-span-2">
-          {searchParams?.address}
+          {searchParams.get("address")}
         </span>
         <Label className="col-span-1">Status</Label>
-        <span className="text-gray-500 col-span-2">{searchParams?.status}</span>
+        <span className="text-gray-500 col-span-2">
+          {registrationStatusToString(searchParams.get("status") as string)}
+        </span>
         <Label className="col-span-1">Ngày đăng ký</Label>
         <span className="text-gray-500 col-span-2">
-          {searchParams?.createdAt && formatDateTimeToddMMyyyyHHmm(searchParams?.createdAt)}
+          {formatDateTimeToddMMyyyyHHmm(
+            searchParams.get("createdAt") as string
+          )}
         </span>
       </div>
       <div className="grid grid-cols-2 gap-3">
-        <Button className="col-span-1 bg-emerald-500 hover:bg-emerald-300">Xác nhận</Button>
-        <Button className="col-span-1 bg-red-500 hover:bg-red-300">Từ chối</Button>
-        <Button className="col-span-2">Liên hệ để lấy thêm thông tin</Button>
+        <Button
+          onClick={handleApprove}
+          disabled={isLoading}
+          className="col-span-1 bg-emerald-500 hover:bg-emerald-300"
+        >
+          Xác nhận
+        </Button>
+        <Button
+          disabled={isLoading}
+          className="col-span-1 bg-red-500 hover:bg-red-300"
+        >
+          Từ chối
+        </Button>
+        <Button disabled={isLoading} className="col-span-2">
+          Liên hệ để lấy thêm thông tin
+        </Button>
       </div>
     </div>
   );
--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -28,6 +28,7 @@ const toastVariants = cva(
     variants: {
       variant: {
         default: "border bg-background text-foreground",
+        success: "border bg-emerald-400 text-white",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
