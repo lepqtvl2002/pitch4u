ed36d56c97830a934eff5301fc5d121b0be52681
lepqtvl2002 <ducthang4825@gmail.com>
2023-11-05 21:45:39 +0700

    improvement landing page

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -1,17 +1,82 @@
-import React from 'react';
-import {cn} from "@/lib/utils";
+import React from "react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Facebook, Twitter } from "lucide-react";
 
-export default function Footer({className, ...props}: { className?: string }) {
-    return (
-        <footer className={cn("flex flex-col md:flex-row justify-between w-full py-10", className)} {...props}>
-            <div className="w-full md:w-1/2">
-                <h2 className="font-bold text-3xl">CONTACT WITH US</h2>
+export default function Footer({
+  className,
+  ...props
+}: {
+  className?: string;
+}) {
+  return (
+    <footer
+      className={cn(
+        "flex flex-col md:flex-row justify-between w-full py-20",
+        className
+      )}
+      {...props}
+    >
+      <div className="w-full md:w-1/2">
+        <h2 className="text-3xl mb-4">Contact with us</h2>
+      </div>
+      <div className="w-full md:w-1/2 text-sm md:text-md">
+        <div className="flex w-full pb-10 space-x-4">
+          <div className="md:w-1/2 text-lg">
+            <h4 className="pb-2 text-sm font-bold mb-4">Chi nhánh chính</h4>
+            <p className="">54, Nguyễn Lương Bằng,</p>
+            <p className="">Hòa Khánh Bắc, Liên Chiểu, Đà Nẵng</p>
+            <p className="">(123) 456 7890</p>
+          </div>
+          <div className="md:w-1/2 text-lg flex flex-col">
+            <h4 className="pb-2 text-sm font-bold mb-4">Social media</h4>
+            <a className="flex underline" href="/">
+              <Facebook className="mr-2" />
+              Facebook
+            </a>
+            <a className="flex underline" href="/">
+              <Twitter className="mr-2" />
+              Twitter
+            </a>
+          </div>
+        </div>
+        <div className="mb-10">
+          <p className="font-bold">Email:</p>
+          <p className="text-lg">example@example.com</p>
+          <p className="font-bold">Phone:</p>
+          <p className="text-lg">012-345-6789</p>
+        </div>
+
+        <p className="font-bold pb-2">Tải app ngay thôi</p>
+        <div className="pr-4">
+          <div className="flex justify-end space-x-10">
+            <div className="flex flex-col justify-around h-32">
+              <a href="/" className="p-2 bg-white rounded shadow">
+                <Image
+                  src="/logo-play-store.png"
+                  alt="logo-play-store"
+                  width={100}
+                  height={80}
+                />
+              </a>
+              <a href="/" className="p-2 bg-white rounded shadow">
+                <Image
+                  src="/logo-app-store.png"
+                  alt="logo-app-store"
+                  width={100}
+                  height={80}
+                />
+              </a>
             </div>
-            <div className="w-full md:w-1/2 text-sm md:text-md">
-                <p>Email: example@example.com</p>
-                <p>Phone: 123-456-7890</p>
-                <p>Address: 123 Example St, Example City, EX 12345</p>
-            </div>
-        </footer>
-    );
+            <Image
+              src="/test-qr-code.jpg"
+              alt="qr-code"
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
