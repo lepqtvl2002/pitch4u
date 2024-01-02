859b25fcde83042c009791d15c671755f92d5773
lepqtvl2002 <ducthang4825@gmail.com>
2024-01-02 21:45:53 +0700

    Add year picker into dashboard overview

--- src/components/notification-badge.tsx
+++ src/components/notification-badge.tsx
@@ -1,14 +1,28 @@
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
-export function NotificationBadge({number = 0, className = ""}: { number: number, className?: string }) {
-    return (
-        <>
-            <span
-                className={cn("absolute top-0 right-0 inline-flex rounded-full h-4 w-4 bg-sky-500 justify-center text-xs text-white font-semibold", className)}>
-                        {!number ? null : number > 9 ? "+9" : number}
-            </span>
-            <span
-                className={cn("absolute top-0 right-0 animate-ping inline-flex h-4 w-4 rounded-full bg-sky-400 opacity-75", className)}></span>
-        </>
-    )
+export function NotificationBadge({
+  number = 0,
+  className = "",
+}: {
+  number: number;
+  className?: string;
+}) {
+  return (
+    <>
+      <span
+        className={cn(
+          "absolute top-0 right-0 inline-flex rounded-full h-4 w-4 bg-sky-500 justify-center text-xs text-white font-semibold",
+          className
+        )}
+      >
+        {!number ? null : number > 9 ? "+9" : number}
+      </span>
+      <span
+        className={cn(
+          "absolute top-0 right-0 animate-ping inline-flex h-4 w-4 rounded-full bg-sky-400 opacity-75",
+          className
+        )}
+      ></span>
+    </>
+  );
 }
