ed48364d278e9298eea7059ace0cfe794155d763
lepqtvl2002 <ducthang4825@gmail.com>
2024-01-14 11:32:37 +0700

    Refactor PitchPage

--- src/app/(general)/pitch/page.tsx
+++ src/app/(general)/pitch/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { roleSlugToString } from "@/lib/utils";
+import { LayoutDashboardIcon, PlusIcon, StepBackIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
@@ -7,9 +10,45 @@ export default function PitchPage() {
   const router = useRouter();
 
   if (status === "loading") return <>Loading...</>;
-  if (session?.user.userRole.name === "admin" || session?.user.userRole.name === "staff") {
-    router.push("/dashboard");
-  } else {
-    router.push("/pitch/register");
-  }
+
+  return (
+    <center className="h-screen flex flex-col">
+      <div className="m-auto">
+        <div className="flex flex-col gap-4 p-4">
+          <h2 className="text-2xl font-medium">
+            Chào mừng bạn đến với trang đăng ký sân
+          </h2>
+          <h4>
+            Bạn đang tham gia hệ thống với vai trò là{" "}
+            <span className="font-semibold text-emerald-500">
+              {roleSlugToString(session?.user.userRole.name ?? "user")}
+            </span>
+          </h4>
+          {session?.user.userRole.name !== "admin" && (
+            <span className="text-muted-foreground italic">
+              Nếu bạn muốn tham gia với vai trò của chủ sân, hãy thực hiện đăng
+              ký sân ở bên dưới
+            </span>
+          )}
+        </div>
+        <div className="flex flex-col gap-2 p-4">
+          <Button
+            onClick={() => router.push("/pitch/register")}
+            className="bg-emerald-500 hover:bg-emerald-300"
+          >
+            <PlusIcon /> Đăng ký sân bóng
+          </Button>
+          {(session?.user.userRole.name === "admin" ||
+            session?.user.userRole.name === "staff") && (
+            <Button className="gap-2" onClick={() => router.push("/dashboard")}>
+              <LayoutDashboardIcon /> Đi tới bảng điều khiển
+            </Button>
+          )}
+          <Button onClick={() => router.back()} variant="outline">
+            <StepBackIcon /> Trở về
+          </Button>
+        </div>
+      </div>
+    </center>
+  );
 }
