20e2d96348e0141fa8d3c0074d48fa9828032497
lepqtvl2002 <ducthang4825@gmail.com>
2024-05-05 11:24:40 +0700

    P4U-210 Update table in the Booking Page

--- src/app/(dashboard)/dashboard/booking/page.tsx
+++ src/app/(dashboard)/dashboard/booking/page.tsx
@@ -3,9 +3,9 @@ import BookingStatCards from "./stat-cards";
 
 export default function BookingPage() {
   return (
-    <div className="container py-4 md:py-10 max-w-7xl">
-        <BookingStatCards />
-      <div className="mx-auto py-10 max-w-screen-2xl">
+    <div className="px-2 md:container py-4 md:py-10 max-w-7xl">
+      <BookingStatCards />
+      <div className="md:mx-auto py-10">
         <BookingTable />
       </div>
     </div>
--- src/app/(dashboard)/dashboard/booking/stat-cards.tsx
+++ src/app/(dashboard)/dashboard/booking/stat-cards.tsx
@@ -5,32 +5,34 @@ import { StatisticUseQuery } from "@/server/queries/statistic-queries";
 export default function BookingStatCards() {
   const { data, isError, isLoading } = StatisticUseQuery.getPitchStats();
 
-  if (isLoading)
-    return (
-      <div className="grid grid-cols-3 gap-4 md:grid-cols-3">
-        <StatCard.Loading />
-        <StatCard.Loading />
-        <StatCard.Loading />
-      </div>
-    );
   if (isError) return <h3>Error</h3>;
   return (
-    <div className="grid grid-cols-3 gap-4 md:grid-cols-3">
-      <StatCard
-        title="Số sân đang quản lý"
-        value={data.result.pitches.length}
-        icon="managerPitch"
-      />
-      <StatCard
-        title="Tổng doanh thu"
-        value={data.result.all.revenue.toLocaleString()}
-        icon="dollar"
-      />
-      <StatCard
-        title="Tổng số lượt đặt sân"
-        value={data.result.all.orders}
-        icon="check"
-      />
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+      {isLoading ? (
+        <>
+          <StatCard.Loading />
+          <StatCard.Loading />
+          <StatCard.Loading />
+        </>
+      ) : (
+        <>
+          <StatCard
+            title="Số sân đang quản lý"
+            value={data.result.pitches.length}
+            icon="managerPitch"
+          />
+          <StatCard
+            title="Tổng doanh thu"
+            value={data.result.all.revenue.toLocaleString()}
+            icon="dollar"
+          />
+          <StatCard
+            title="Tổng số lượt đặt sân"
+            value={data.result.all.orders}
+            icon="check"
+          />
+        </>
+      )}
     </div>
   );
 }
